e061601c1e9189dfc1f994c4bd3e6f0372aa986f
Alejandra Izaguirre <aleizatebar@gmail.com>
2020-03-29 15:33:55 +0200

    Passing multiple props to a component

--- src/index.js
+++ src/index.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-
+import faker from 'faker';
 /**
  * to import one of our components in our project we have to indicate 
  * the route to the file. It doesn't have to have the '.js' termination
@@ -10,9 +10,18 @@ import CommentDetail from './CommentDetail';
 const App = () => {
     return (
         <div className="ui container comments">
-            <CommentDetail author="Sam"/>
-            <CommentDetail author="Alex"/>
-            <CommentDetail author="Jane"/>
+            <CommentDetail author="Sam"
+                timeAgo="Today at 4:45PM"
+                commentText="Great course!"
+                avatar={faker.image.avatar()} />
+            <CommentDetail author="Alex"
+                timeAgo="Today at 2:00PM"
+                commentText="Nice blog" 
+                avatar={faker.image.avatar()}/>
+            <CommentDetail author="Jane"
+                timeAgo="Today at 5:00PM"
+                commentText="Could you do another one?" 
+                avatar={faker.image.avatar()}/>
         </div>
     );
 }
